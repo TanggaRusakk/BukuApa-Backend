dd47a11756fd43b967161cf985cda6a3cb4eb824
JesCe303 <jasonchristopher152@gmail.com>
2026-06-04 12:54:06 +0700

    .

--- src/validations/borrowing-validation.ts
+++ src/validations/borrowing-validation.ts
@@ -1,7 +1,9 @@
-import { z, ZodType } from "zod";
+import { z } from "zod";
 
 export class BorrowingValidation {
+    // FIX: Hapus ": ZodType" biar TypeScript bisa ngebaca isinya secara otomatis
     static readonly CREATE = z.object({
-        bookId: z.number().positive("Book ID must be a positive number")
+        bookId: z.number().positive("Book ID must be a positive number"),
+        userId: z.number().positive("User ID must be a positive number").optional()
     });
 }
