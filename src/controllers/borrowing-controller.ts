ab500dbb6d541b6baa9d23504023d2c3a342eb51
JesCe303 <jasonchristopher152@gmail.com>
2026-06-04 14:11:26 +0700

    .

--- src/controllers/borrowing-controller.ts
+++ src/controllers/borrowing-controller.ts
@@ -1,30 +1,46 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
+import { UserRequest } from "../models/user-request-model";
 import { BorrowingService } from "../services/borrowing-service";
 
-export interface UserRequest extends Request {
-  user?: any;
-}
-
 export class BorrowingController {
-  static async create(req: UserRequest, res: Response, next: NextFunction) {
-    try {
-      const request = {
-        bookId: req.body.bookId,
-      };
-      const result = await BorrowingService.create(req.user, request);
-      res.status(200).json({ data: result });
-    } catch (e) {
-      next(e);
+    static async create(req: UserRequest, res: Response, next: NextFunction) {
+        try {
+            const request = req.body;
+            const response = await BorrowingService.create(req.user!, request);
+            
+            // WAJIB dibungkus "data"
+            res.status(200).json({
+                data: response
+            });
+        } catch (e) {
+            next(e);
+        }
     }
-  }
 
-  static async returnBook(req: UserRequest, res: Response, next: NextFunction) {
-    try {
-      const loanId = parseInt(req.params.loanId as string);
-      const result = await BorrowingService.returnBook(req.user, loanId);
-      res.status(200).json({ data: result });
-    } catch (e) {
-      next(e);
+    static async returnBook(req: UserRequest, res: Response, next: NextFunction) {
+        try {
+            const loanId = Number(req.params.loanId);
+            const response = await BorrowingService.returnBook(req.user!, loanId);
+            
+            // WAJIB dibungkus "data"
+            res.status(200).json({
+                data: response
+            });
+        } catch (e) {
+            next(e);
+        }
     }
-  }
+
+    static async list(req: UserRequest, res: Response, next: NextFunction) {
+        try {
+            const response = await BorrowingService.list(req.user!);
+            
+            // WAJIB dibungkus "data"
+            res.status(200).json({
+                data: response 
+            });
+        } catch (e) {
+            next(e);
+        }
+    }
 }
